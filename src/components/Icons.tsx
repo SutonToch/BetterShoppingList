1bccdc9072cb5adf4de8d3b141d7abab7c760f3d
SutonToch <77286229+SutonToch@users.noreply.github.com>
2024-07-17 13:43:02 +0200

    UPDATE minor style change

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -8,7 +8,7 @@ interface PlusProps {
 
 export function VerticalDots() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon-btn icon-tabler-dots-vertical">
+        <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-btn icon-tabler-dots-vertical">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
           <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
@@ -19,7 +19,7 @@ export function VerticalDots() {
 
 export function ArrowLeft() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon-btn icon-tabler-arrow-left">
+    <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-btn icon-tabler-arrow-left">
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M5 12l14 0" />
       <path d="M5 12l6 6" />
@@ -30,7 +30,7 @@ export function ArrowLeft() {
 
 export function Edit() {
   return (
-      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon-btn icon-tabler-edit">
+      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-btn icon-tabler-edit">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
         <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
@@ -41,7 +41,7 @@ export function Edit() {
 
 export function Plus(props:PlusProps) {
   return (
-      <svg xmlns="http://www.w3.org/2000/svg" width={props.size} height={props.size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon-btn icon-tabler-plus">
+      <svg xmlns="http://www.w3.org/2000/svg" width={props.size} height={props.size} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-btn icon-tabler-plus">
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M12 5l0 14" />
         <path d="M5 12l14 0" />
@@ -51,7 +51,7 @@ export function Plus(props:PlusProps) {
 
 export function Delete() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon-btn icon-tabler-delete">
+    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-btn icon-tabler-delete">
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 7l16 0" />
       <path d="M10 11l0 6" />
